3173328997611190ec535404ec86634533aea364
Darwaivaibhav123 <91363956+Darwaivaibhav123@users.noreply.github.com>
2022-04-08 20:03:38 +0530

    Update and rename README.md to addition of two no.cpp

---
+++ addition of two no.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std ;
+int main ()
+{
+    
+    int a ,b ,sum ,  substration, avg ;
+    cout<< "enter the first  two  no = ";
+    cin>> a ;
+    cin>> b ;
+    sum = a +b ;
+    avg= sum/2; 
+    substration = a-b;
+    cout<< "sum of two no are = "<<sum<<endl ;
+     cout<< "sum of two no are = "<< avg<<endl ;
+    cout<< "sum of two no are = "<< substration<<endl ;
+    cout<< "hello world";
+    return 0 ;
+}
