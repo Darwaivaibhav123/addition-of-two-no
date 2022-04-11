c89eb73fcb27acb452a704c9217ff35ce7a8356c
Darwaivaibhav123 <91363956+Darwaivaibhav123@users.noreply.github.com>
2022-04-11 19:44:05 +0530

    Create sum of first 100 nature no.cpp

---
+++ sum of first 100 nature no.cpp
@@ -0,0 +1,17 @@
+// program of addittion of first 100 numbers 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i, n ,sum  ;
+     sum = 0 ;
+  for(i=1;i <100 ; i++){
+      
+      
+     sum = sum + i ;
+ } 
+  cout<< "the sum of first hundrad numbers  are = "<<sum<<endl;
+    return 0;
+}
