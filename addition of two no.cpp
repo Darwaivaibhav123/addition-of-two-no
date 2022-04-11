95fb941ad40dd044ef16cc1e3ea7efdc0ca3e690
Darwaivaibhav123 <91363956+Darwaivaibhav123@users.noreply.github.com>
2022-04-11 10:22:56 +0530

    Update addition of two no.cpp

--- addition of two no.cpp
+++ addition of two no.cpp
@@ -16,3 +16,59 @@ int main ()
     cout<< "hello world";
     return 0 ;
 }
+//Print the no that enter by users
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int a;
+  cout<< "enter the no = ";
+  cin>>a ;
+  cout<< "print the no that you are enterd ="<<a ;
+  }
+//revers the two no 
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+  int a , b,temp ;
+  cout << "enter a first no ="<<endl;
+  cin>>a ;
+  cout << "enter a second no = "<<endl;
+  cin>>b;
+  temp=a;
+  a=b;
+  b=temp;
+
+  cout<<"new value of a is ="<<a;
+
+cout<<"new value of b is="<<b;
+  return 0;
+}
+//find even and odd no 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a  ;
+  cout << "enter a  no =";
+  cin>>a ;
+  if (a%2==0  )
+  cout<<"given no is even ";
+  else if (a%2!=0)
+  cout<<"given no is odd " ;
+   else if  (a==0 ) 
+   cout << "given no is 0";
+  return 0;
+}
+
+
+
+
