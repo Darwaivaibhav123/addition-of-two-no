a0421a5596a9f5a51ac157f10ad6cdc09239bd83
Darwaivaibhav123 <91363956+Darwaivaibhav123@users.noreply.github.com>
2022-04-11 19:45:01 +0530

    Create  prime no.cpp

---
+++ prime no.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+      int a  ;
+  cout << "enter a  no =";
+  cin>>a ;
+  if (a==1 )
+  cout << "given no is not prime no";
+
+   else if (a==2 )
+  cout<<"given no is prime no ";
+  else if (a%2!=0  )
+  cout<<"given no is prime no ";
+   else   
+   cout << "given no is not prime no";
+
+    return 0;
+}
